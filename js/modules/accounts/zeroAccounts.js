5177af7df13895a76b52fb52238675087fc5829f
htmllancer <htmllancer@yandex.ru>
2013-09-05 17:27:03 +0400

    ZeroAccounts

---
+++ js/modules/accounts/zeroAccounts.js
@@ -0,0 +1,163 @@
+Zero.GoogleAccount = (function(module){
+	var m = {}, accountHolder, 	tokkens = module.getTokens(), g_account, nike_account, fitbit_account;
+	
+	_getGoogleAuthUrl = function() {
+		
+		try{
+			$.ajax({
+				beforeSend: function (request) {
+					request.setRequestHeader("Access-Token", tokkens.accessToken);
+				},							
+				url: initConfiguration.urlGoogleAuthorizationURL,
+				type: 'GET',
+				data : {
+					mode : 'web'
+				},
+				dataType: 'json',
+				contentType: "application/json",
+				success: function (resp) {		
+					var params = {
+						text : 'Add Google Account',
+						title : 'Google Account',
+						type : 'Google'
+						
+					}				
+					_drawGoogleAuthButton(resp.authorizationURL, params)
+				},
+				error : function(error) {
+					console.log(error);
+				}
+			})		
+		}catch(e){
+			console.log(e);
+		};			
+	}
+	
+	_getFitBitAuthUrl = function() {		
+		try{
+			$.ajax({
+				beforeSend: function (request) {
+					request.setRequestHeader("Access-Token", tokkens.accessToken);
+				},							
+				url: initConfiguration.urlFitBitAuthorizationURL,
+				type: 'GET',
+				dataType: 'json',
+				contentType: "application/json",
+				success: function (resp) {	
+					var params = {
+						text : 'Add Fitbit Account',
+						className : 'fitbit',
+						title : 'Fitbit Account',
+						type : 'FitBit'
+					}
+					_drawGoogleAuthButton(resp.authorizationURL, params);
+					_getNikeConnectHtml();
+				},
+				error : function(error) {
+					console.log(error);
+				}
+			})		
+		}catch(e){
+			console.log(e);
+		};			
+	}	
+	
+	_getNikeConnectHtml = function() {
+		var html = $('<div />').addClass('account-item'),
+			header = $('<h2/>').text('Nike Connect'),
+			step1 = $('<p/>').html("Login with you email and password:<a></a>"),
+			text = $('<p />').text('Please point your Nike Access token in field below'),
+			nikeTokken = $('<input />').attr({
+				'id' : 'nikeTokken',
+			}),
+			bt = $('<button />').text('Connect');
+			
+			header.appendTo(html);
+			text.appendTo(html);
+			nikeTokken.appendTo(html);
+			bt.appendTo(html);
+			
+			bt.bind('click', _getNikeConnect);
+			
+			html.appendTo(accountHolder);
+	}
+	
+	_getNikeConnect = function() {
+		//getNikeConnect
+		
+		var tk = $('#nikeTokken').val();
+
+		if(!tk) return;
+
+		try{
+			$.ajax({
+				beforeSend: function (request) {
+					request.setRequestHeader("Access-Token", tokkens.accessToken);
+				},							
+				url: initConfiguration.urlNikeAuthorizationURL + '/?accessToken=' + tk,
+				type: 'GET',
+				dataType: 'json',
+				contentType: "application/json",
+				success: function (resp) {	
+					console.warn(resp)
+				},
+				error : function(error) {
+					console.log(error);
+				}
+			})		
+		}catch(e){
+			console.log(e);
+		};		
+	}
+	
+	_analyzeAccounts = function() {
+		var accounts = initConfiguration.settingsData.accounts;
+		
+		for(var i = 0; i < accounts.length; i++) {
+		
+		}
+		
+		
+	};
+	
+	
+	_setAccountHolder = function(holder) {
+		accountHolder = holder;
+		_analyzeAccounts();
+		_getGoogleAuthUrl();
+		_getFitBitAuthUrl();
+	}
+	
+	_drawGoogleAuthButton = function(link, params) {	
+		var html = $('<div />').addClass('account-item');
+		var header = $('<h2 />').text(params.title)
+		var $bt = $('<button />').data('google-link', link).text(params.text);
+		if(params.className) { 
+			$bt.addClass(params.className)
+		}
+		$bt.bind('click', function(e) {
+			var params = "menubar=no,location=yes,resizable=yes,scrollbars=yes,status=yes,width=500,height=500, top=100, left=100";
+			window.open($(this).data('google-link'), params);
+			e.preventDefault();
+		})	
+		
+		if(params.title && params.title != '') {
+			header.appendTo(html);
+		}
+		$bt.appendTo(html);
+		html.appendTo(accountHolder);
+		
+	}
+	
+	
+	m.closeConnectWindow = function(w) {
+		w.close();
+		console.warn('page closed');
+	}
+	
+	m.init = function(holder) {		
+		_setAccountHolder(holder);
+	}
+	
+	return m;
+}(Zero));
---
+++ js/modules/charts/YesterdayChartController.js
@@ -0,0 +1,52 @@
+Zero.YesterdayController = (function(module){
+	var view = {},
+	
+	    config = {
+	    	title:"YESTERDAY'S RECAP",
+	    	subTitle:"You're Reached Your Activity Goal!",
+	    	article1:"You walked 17,000 steps yesterday! That is 12% better than your best day of 15,320 steps on July 24th, 2013.",
+	    	article2:"Take a moment to think about what you did and recreate that every day",
+	    	chart:"yes-chart.png"
+	    	
+	    },
+	
+	    _render = function(){
+	       _postRender();
+	    },
+	    
+	    _postRender = function(data){
+	       _paintYesterday();
+	       // _handlers();
+
+	    },
+	    
+	    _handlers = function(){
+	    	
+	    },
+	    
+	    _paintYesterday = function(){
+	    	var root = initConfiguration.getRootLocation();
+	       var wrap = $('.chart-yesterday');
+	       var h1yes = $('<h1/>').addClass('header-yes').text(config.title);
+	       var subH2 = $('<h2/>').text(config.subTitle);
+	       var chartImg = $('<img/>').attr('src', root+initConfiguration.imagesFolder+config.chart);
+	       var yesChart = $('<div/>').addClass('wrap-p');
+	       var pArticle1 = $('<p/>').text(config.article1);
+	       var pArticle2 = $('<p/>').text(config.article2);
+	       
+	       yesChart.append(pArticle1);
+	       yesChart.append(pArticle2);
+	       
+	       wrap.append(h1yes);
+	       wrap.append(subH2);
+	       wrap.append(chartImg);
+	       wrap.append(yesChart);
+	    };
+	    
+	    
+	view.initialize = function(){
+		_render();
+	};
+	
+	return view;
+}(Zero));
