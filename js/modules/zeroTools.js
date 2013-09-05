ab83c32e264b1b3a57ea22b21c3b5811d2f158ee
htmllancer <htmllancer@yandex.ru>
2013-09-05 19:14:03 +0400

    fix seconds

--- js/modules/zeroTools.js
+++ js/modules/zeroTools.js
@@ -13,7 +13,15 @@ Zero.Tools = (function(module){
 		if(month < 10) {
 			month = '0' + month;
 		}		
-		var formattedTime = day + '.' + month + '.' + year + ' ' + hours + ':' + minutes + ':' + seconds;	
+		
+		if(hours < 10) {
+			hours = '0' + hours;
+		}				
+		if(minutes < 10) {
+			minutes = '0' + minutes;
+		}						
+		
+		var formattedTime = day + '.' + month + '.' + year + ' ' + hours + ':' + minutes;	
 		return formattedTime;	
 	}	
 	
