52ffb2cdcce9ea6ba6d8fd816e69ccbb0f624e76
SergeySukhanov <sergey.n.sukhanov@gmail.com>
2013-09-20 16:11:47 +0400

    new config

--- js/initConfiguration.js
+++ js/initConfiguration.js
@@ -13,38 +13,42 @@ var initConfiguration = {
 	 	return initConfiguration.urlCharts;
 	 },
 	 
-	 //auth
-	urlRegister:'http://zerosrv02.domain.corp/api_usr/V1/register/w',
-	urlLogin:'http://zerosrv02.domain.corp/api_usr/V1/login',
-	
-	//set data charts
-	urlCharts:'http://zerosrv02.domain.corp/api_srv/V1/getChartsData',
-	urlCalendarsList:'http://zerosrv02.domain.corp/api_srv/V1/calendars',
-	urlEventsCalendar:'http://zerosrv02.domain.corp/api_srv/V1/calendarEvents',
-	
-	//Account	
-	urlGoogleAuthorizationURL : 'http://zerosrv02.domain.corp/api_srv/V1/getGoogleAuthorizationURL',
-	urlFitBitAuthorizationURL : 'http://zerosrv02.domain.corp/api_srv/V1/getFitbitAuthorizationURL',
-	urlNikeAuthorizationURL : 'http://zerosrv02.domain.corp/api_srv/V1/getNikeConnect',
-	
-	//settings
-	urlSettings:'http://zerosrv02.domain.corp/api_srv/V1/settings',
-	urlSliders:'http://zerosrv02.domain.corp/api_srv/V1/willpower/sliders',
-    urlWillpower:'http://zerosrv02.domain.corp/api_srv/V1/willpower/data',
-/*    urlRegister:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/register/w',
+	 // //auth
+	// urlRegister:'http://zerosrv02.domain.corp/api_usr/V1/register/w',
+	// urlLogin:'http://zerosrv02.domain.corp/api_usr/V1/login',
+// 	
+	// //set data charts
+	// urlCharts:'http://zerosrv02.domain.corp/api_srv/V1/getChartsData',
+	// urlCalendarsList:'http://zerosrv02.domain.corp/api_srv/V1/calendars',
+	// urlEventsCalendar:'http://zerosrv02.domain.corp/api_srv/V1/calendarEvents',
+// 	
+	// //Account	
+	// urlGoogleAuthorizationURL : 'http://zerosrv02.domain.corp/api_srv/V1/getGoogleAuthorizationURL',
+	// urlFitBitAuthorizationURL : 'http://zerosrv02.domain.corp/api_srv/V1/getFitbitAuthorizationURL',
+	// urlNikeAuthorizationURL : 'http://zerosrv02.domain.corp/api_srv/V1/getNikeConnect',
+// 	
+	// //settings
+	// urlSettings:'http://zerosrv02.domain.corp/api_srv/V1/settings',
+	// urlSliders:'http://zerosrv02.domain.corp/api_srv/V1/willpower/sliders',
+    // urlWillpower:'http://zerosrv02.domain.corp/api_srv/V1/willpower/data',
+
+    urlRegister:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/register/w',
 	urlLogin:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/login',
 	
-	urlCharts:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/getChartsData',
-	urlCalendarsList:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/calendars',
-	urlEventsCalendar:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/calendarEvents',
+	urlCharts:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/getChartsData',
+	urlCalendarsList:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/calendars',
+	urlEventsCalendar:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/calendarEvents',
 
-	urlGoogleAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/getGoogleAuthorizationURL',
-	urlFitBitAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/getFitbitAuthorizationURL',
-	urlNikeAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/getNikeConnect',
+	urlGoogleAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/getGoogleAuthorizationURL',
+	urlFitBitAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/getFitbitAuthorizationURL',
+	urlNikeAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/getNikeConnect',
 	
-	urlSettings:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/settings',
-	urlWillpower:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/willpower',   
-  */
+	urlSettings:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/settings',
+	urlSliders:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/willpower/sliders',
+	urlWillpower:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/willpower/data',
+   
+  
+
 	
 	
 	
