8a4ecde5f42833146a474d7dc8e5a5f51691b3dd
htmllancer <htmllancer@yandex.ru>
2013-09-25 12:47:15 +0400

    adding api url

--- js/initConfiguration.js
+++ js/initConfiguration.js
@@ -12,8 +12,10 @@ var initConfiguration = {
 	 getChartsData:function(){
 	 	return initConfiguration.urlCharts;
 	 },
-//LOCAL	 
+	 
 	 //auth
+	 
+	apiUrl: 'http://zerosrv02.domain.corp/api_usr/V1/', 
 	urlRegister:'http://zerosrv02.domain.corp/api_usr/V1/register/w',
 	urlLogin:'http://zerosrv02.domain.corp/api_usr/V1/login',
 	
@@ -31,48 +33,20 @@ var initConfiguration = {
 	urlSettings:'http://zerosrv02.domain.corp/api_srv/V1/settings',
 	urlSliders:'http://zerosrv02.domain.corp/api_srv/V1/willpower/sliders',
     urlWillpower:'http://zerosrv02.domain.corp/api_srv/V1/willpower/data',
-    urlWeather:'http://zerosrv02.domain.corp/api_srv/V1/weather',
-
-//DEV
- /*
-    urlRegister:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/register/w',
+/*    urlRegister:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/register/w',
 	urlLogin:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/login',
 	
-	urlCharts:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/getChartsData',
-	urlCalendarsList:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/calendars',
-	urlEventsCalendar:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/calendarEvents',
+	urlCharts:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/getChartsData',
+	urlCalendarsList:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/calendars',
+	urlEventsCalendar:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/calendarEvents',
 
-	urlGoogleAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/getGoogleAuthorizationURL',
-	urlFitBitAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/getFitbitAuthorizationURL',
-	urlNikeAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/getNikeConnect',
+	urlGoogleAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/getGoogleAuthorizationURL',
+	urlFitBitAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/getFitbitAuthorizationURL',
+	urlNikeAuthorizationURL : 'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/getNikeConnect',
 	
-	urlSettings:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/settings',
-	urlSliders:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/willpower/sliders',
-	urlWillpower:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/willpower/data',
-    urlWeather:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_srv/V1/weather',
-*/	
-	
-//DEMO	
-	/*
-	urlRegister:'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_usr/V1/register/w',
-	urlLogin:'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_usr/V1/login',
-	
-	urlCharts:'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/getChartsData',
-	urlCalendarsList:'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/calendars',
-	urlEventsCalendar:'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/calendarEvents',
-
-	urlGoogleAuthorizationURL : 'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/getGoogleAuthorizationURL',
-	urlFitBitAuthorizationURL : 'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/getFitbitAuthorizationURL',
-	urlNikeAuthorizationURL : 'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/getNikeConnect',
-	
-	urlSettings:'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/settings',
-	urlSliders:'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/willpower/sliders',
-	urlWillpower:'http://ec2-54-216-166-33.eu-west-1.compute.amazonaws.com/api_srv/V1/willpower/data',
-   */
-
-
-  
-
+	urlSettings:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/settings',
+	urlWillpower:'http://ec2-54-216-165-90.eu-west-1.compute.amazonaws.com/api_usr/V1/willpower',   
+  */
 	
 	
 	
