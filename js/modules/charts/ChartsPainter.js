483eeb5b7487209f48c97efd5d7f585fd6b96b1c
SergeySukhanov <sergey.n.sukhanov@gmail.com>
2013-10-10 15:50:40 +0400

    events for charts

--- js/modules/charts/ChartsPainter.js
+++ js/modules/charts/ChartsPainter.js
@@ -53,7 +53,7 @@ function onJsGraphDataLoad(holder, series, flag) {
 
 					colors : ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce', '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'],
 					rangeSelector:{
-						enabled : true,
+						enabled : false,
 						selected:0,
 						inputEnabled:false,
 						buttons: [{
